b139ef6ccb5fed9f10487164f0e0eee65d5787e8
Chris <chris.cornelison@gmail.com>
2016-04-05 20:45:01 -0500

    limit function

--- amdahl/src/amdahl/core.clj
+++ amdahl/src/amdahl/core.clj
@@ -1,18 +1,30 @@
 (ns amdahl.core
-  (:require [clojure.math.numeric-tower :refer [expt]])
+  (:require [clojure.math.numeric-tower :refer [abs expt]])
   (:gen-class))
 
-
-(def p 0.6)
 
 (defn amdahl
   [p n]
   (/ 1 (+ (- 1 p) (/ p n))))
 
-(amdahl p n)
 
-; find 
-(map #(amdahl p (expt 10 %)) (range 1 32))
+(defn limit-inf
+  "Find the limit numerically of the function f as n goes to infinity"
+  [f]
+  (let [precision 0.0000000000001
+        g (fn [f last-value n]
+            (let [new-value (f n)]
+              (if (< (abs (- last-value new-value)) precision)
+                new-value
+                (recur f new-value (* n 2)))))]
+    (g f (f 1) 2)))
+
+
+(defn exercise-6-1 []
+  (let [p 0.6
+        f (partial amdahl p)]
+    (limit-inf f)))
+
 
 
 (defn -main
